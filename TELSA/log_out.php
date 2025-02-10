9bed06dfc9987bba6c2fb51825b3431639e4e284
alobiine <alobiinenuwasious@gmail.com>
2025-02-10 15:27:58 +0300

    Add TESLA from local machine

---
+++ TELSA/log_out.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOG OUT</title>
+</head>
+<body>
+    <div>
+        <h1>LOG OUT PAGE</h1>
+    </div>
+    <div>
+        <form>
+            <div>
+                <label>USER NAME</label>
+                <input type="text" name="username" placeholder="username">
+            </div>
+            <div>
+                <label>EMAIL </label>
+                <input type="email" name="email" placeholder="user@gmail.com">
+            </div>
+            <div>
+                <label>PASSWORD </label>
+                <input type="password" name="password" placeholder="password">
+            </div>
+            <div>
+                <button type="submit">LOG OUT</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
